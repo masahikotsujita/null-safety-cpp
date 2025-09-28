e574c43660e238e83e61a0c3f39c146847e9ca15
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2025-09-28 16:44:19 +0900

    nullable_ptr.hとnonnull_ptr.hをpointerディレクトリに移動

--- include/null-safety/null-safety.h
+++ include/null-safety/null-safety.h
@@ -5,7 +5,7 @@
 #ifndef NULL_SAFETY_CPP_NULL_SAFETY_H
 #define NULL_SAFETY_CPP_NULL_SAFETY_H
 
-#include "nonull_ptr.h"
-#include "nullable_ptr.h"
+#include "pointer/nonull_ptr.h"
+#include "pointer/nullable_ptr.h"
 
 #endif //NULL_SAFETY_CPP_NULL_SAFETY_H
